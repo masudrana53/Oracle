23ef7d60295f72dbbb31ed34e5f62d2ca512839b
Masud Rana <masudrana.20091@gmail.com>
2023-07-04 13:17:31 +0600

    add

---
+++ Table create.sql
@@ -0,0 +1,36 @@
+CREATE TABLE masud_1277593(
+
+eid int primary key,
+ename VARCHAR2(30),
+salary NUMBER(8,2),
+hire_date DATE,
+address VARCHAR2(50)
+);
+
+INSERT INTO masud_1277593 VALUES(
+1, 'MASUD', 105, '01/MAR/1996', 'UTTARA');
+
+INSERT INTO masud_1277593 VALUES(
+2, 'RANA', 106, '02/APR/1997', 'KERANIGONJ');
+
+INSERT INTO masud_1277593 VALUES(
+3, 'HASAN', 107, '03/MAY/1998', 'MIRPUR');
+
+INSERT INTO masud_1277593 VALUES(
+4, 'JAHIR', 108, '04/JUN/1999', 'GULSHAN');
+
+INSERT INTO masud_1277593 VALUES(
+5, 'TAMIM', 108, '05/JUL/2000', 'BANANI');
+
+
+UPDATE masud_1277593
+SET
+salary= null,
+address= 'DHAKA'
+WHERE eid=3;
+
+
+SELECT* 
+FROM masud_1277593;
+
+commit;
